644db63277f98a9a3e98193a6e5fc7bfbecf14a1
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-07 10:20:23 -0300

    2.5 - Utilizando as expressões

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -3,9 +3,30 @@ package br.com.alura.bytebank
 import br.com.alura.bytebank.modelo.Endereco
 import java.lang.ArithmeticException
 import java.lang.ClassCastException
+import java.lang.NumberFormatException
 
 fun main() {
     println("início main")
+    val entrada: String = "1.0"
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException){
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null){
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null){
+        println("Valor recebido $valorComTaxa")
+    } else{
+        println("Valor inválido")
+    }
     funcao1()
     println("fim main")
 }
