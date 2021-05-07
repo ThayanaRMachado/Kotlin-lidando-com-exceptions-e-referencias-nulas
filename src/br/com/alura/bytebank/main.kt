e61758f346c3a0470199ce4284af92abcd08ba45
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-07 15:05:34 -0300

    5.5 - Outros recursos do null safety

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -5,11 +5,21 @@ import br.com.alura.bytebank.modelo.Endereco
 import br.com.alura.bytebank.teste.testaComportamentosConta
 import java.lang.ArithmeticException
 import java.lang.Exception
+import java.lang.IllegalStateException
 
 fun main() {
-    var enderecoNulo: Endereco? = Endereco(logradouro = "rua Vergueiro")
+    var enderecoNulo: Endereco? = Endereco(logradouro = "rua Vergueiro", complemento = "prédio")
     val logradouroNovo: String? = enderecoNulo?.logradouro
-    enderecoNulo?.let { endereco: Endereco ->
-        println(endereco.logradouro.length)
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+        println(tamanhoComplemento)
     }
+    teste(1)
+    teste("")
+}
+
+fun teste(valor: Any){
+    val numero: Int? = valor as? Int
 }
