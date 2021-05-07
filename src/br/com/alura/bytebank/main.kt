19037442cc756fabb4bd9e65dfdf63181b6aacd8
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-07 11:38:07 -0300

    3.3 - Criando exceptions

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,6 +1,8 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import java.lang.ArithmeticException
+import java.lang.Exception
 
 fun main() {
     println("início main")
@@ -12,9 +14,9 @@ fun funcao1() {
     println("início funcao1")
     try {
         funcao2()
-    } catch (e: ClassCastException) {
+    } catch (e: SaldoInsuficienteException) {
         println(e.printStackTrace())
-        println("ClassCastException foi pegada")
+        println("SaldoInsuficienteException foi pegada")
     }
     println("fim funcao1")
 }
@@ -23,8 +25,7 @@ fun funcao2() {
     println("início funcao2")
     for (i in 1..5) {
         println(i)
-        val endereco = Any()
-        throw ArithmeticException()
+        throw SaldoInsuficienteException()
     }
     println("fim funcao2")
 }
